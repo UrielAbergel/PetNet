223195e0ad54e8ca3ba21eb7798d9a377221f836
urielabergel <myfliper2@gmail.com>
2020-10-27 18:49:38 +0200

    add small change to main activity

--- app/src/main/java/com/example/petnet/MainActivity.java
+++ app/src/main/java/com/example/petnet/MainActivity.java
@@ -20,10 +20,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        User_Name_input = (EditText) findViewById(R.id.UserNameInput);
-        Password_input = (EditText) findViewById(R.id.PasswordInput);
+        User_Name_input = (EditText) findViewById(R.id.TI_username);
+        Password_input = (EditText) findViewById(R.id.TI_password);
 
-        
+
 
     }
 
