abb82ad058d9433e81c174d7f33b7a22cf091b8f
OrianHindi <orianhindi44@gmail.com>
2020-10-27 23:41:34 +0200

    start register page UI

---
+++ app/src/main/java/com/example/petnet/SignUpPage.java
@@ -0,0 +1,20 @@
+package com.example.petnet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SignUpPage extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+    }
+
+}
