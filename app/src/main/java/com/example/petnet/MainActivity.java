a1e284f60c54d2467fad26ba70f9d62a13422d7b
urielabergel <myfliper2@gmail.com>
2020-10-27 18:48:09 +0200

    add files to the log screen

--- app/src/main/java/com/example/petnet/MainActivity.java
+++ app/src/main/java/com/example/petnet/MainActivity.java
@@ -3,14 +3,29 @@ package com.example.petnet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    String User_Name , Password;
+    Button getResult;
+
+    EditText User_Name_input;
+    EditText Password_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        User_Name_input = (EditText) findViewById(R.id.UserNameInput);
+        Password_input = (EditText) findViewById(R.id.PasswordInput);
+
+        
+
     }
+
+
 }
