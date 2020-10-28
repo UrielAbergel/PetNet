3201435a3dac21204827ab50b01e40088f48ddf6
OrianHindi <orianhindi44@gmail.com>
2020-10-28 18:27:17 +0200

    try add map to register

--- app/src/main/java/com/example/petnet/SignUpPage.java
+++ app/src/main/java/com/example/petnet/SignUpPage.java
@@ -1,6 +1,19 @@
 package com.example.petnet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.os.Bundle;
 import android.view.View;
