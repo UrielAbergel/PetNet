8879ff25c3a73e61b4b604d1599a2e5006028a6b
OrianHindi <orianhindi44@gmail.com>
2020-10-30 11:45:56 +0200

    add pet details & signup button listener(not finished)

--- app/src/main/java/com/example/petnet/SignUpPage.java
+++ app/src/main/java/com/example/petnet/SignUpPage.java
@@ -7,7 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 import android.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class SignUpPage extends AppCompatActivity {
 
@@ -21,12 +26,15 @@ public class SignUpPage extends AppCompatActivity {
     ImageButton hide_confirm_pass;
     androidx.appcompat.widget.Toolbar toolbar;
 
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+        mAuth=FirebaseAuth.getInstance();
         toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -39,15 +47,31 @@ public class SignUpPage extends AppCompatActivity {
         confirm_passwrod = findViewById(R.id.PT_register_confirm_password);
         hide_confirm_pass = findViewById(R.id.IV_hide_confirm_pass);
         sign_up_button = findViewById(R.id.B_signup);
+        email=findViewById(R.id.PT_register_email);
 
 
 
         sign_up_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //validate email
+                if(password.getText().toString()!=confirm_passwrod.getText().toString()) {
 
-            }
+                    Toast.makeText(getApplicationContext(), "Password does not equals.", Toast.LENGTH_LONG).show();
+                    password.setError("Passwords dont match");
+                    password.requestFocus();
+                    return;
+                }
+                String mail = email.getText().toString();
+                String pass = password.getText().toString();
+             try{
+                 mAuth.createUserWithEmailAndPassword(mail,pass); // need to add oncompelte listener and check the exceptions.
+
+             }catch (Exception e){
+
+                }
+             }
+
+
         });
 
 
