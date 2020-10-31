1d4841f57d028fee8437c42c9a10c5afb4f381b6
urielabergel <myfliper2@gmail.com>
2020-11-01 00:57:04 +0200

    config email add Mail api to the system by auto send dialog

---
+++ app/src/main/java/com/example/petnet/Mail_Active.java
@@ -0,0 +1,56 @@
+package com.example.petnet;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class Mail_Active extends AppCompatDialogFragment {
+
+    private EditText get_text_msg;
+    private EditText get_subject;
+    private String text_subject;
+    private String text_msg;
+
+    public Dialog onCreateDialog(Bundle saveInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.mail_active,null);
+        get_text_msg = view.findViewById(R.id.Mail_getText);
+        get_subject = view.findViewById(R.id.Mail_subject);
+        builder.setView(view).setTitle("Contact Us!").setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        }).setPositiveButton("Send", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                text_msg = get_text_msg.getText().toString();
+                text_subject = get_subject.getText().toString();
+
+                sendEmail();
+            }
+        });
+
+        return builder.create();
+    }
+
+    private void sendEmail() {
+
+        JavaMailAPI sendAPI = new JavaMailAPI(this.getContext(),"petnetcontact@gmail.com",text_subject,text_msg);
+        sendAPI.execute();
+    }
+
+
+}
+
--- app/src/main/java/com/example/petnet/MainActivity.java
+++ app/src/main/java/com/example/petnet/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText User_Name_input;
     private EditText Password_input;
     private TextView Sign_up_input;
+    private ImageView FAB_mail_input;
 
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
@@ -44,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         User_Name_input = (EditText) findViewById(R.id.TI_username);
         Password_input = (EditText) findViewById(R.id.TI_password);
         Sign_up_input =  (TextView)  findViewById(R.id.TV_signup);
+        FAB_mail_input = (ImageView) findViewById(R.id.FAB_mail);
 
         //take instant to sign up page
         Sign_up_input.setOnClickListener(new View.OnClickListener() {
@@ -54,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        //if
+        //check log in button and log in into the app
         login_button = (Button) findViewById(R.id.B_login);
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,32 +65,56 @@ public class MainActivity extends AppCompatActivity {
                 User_Name = User_Name_input.getText().toString();
                 Password = Password_input.getText().toString();
 
-                mAuth.signInWithEmailAndPassword(User_Name, Password)
-                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    System.out.println("sss");
-                                    FirebaseUser user = mAuth.getCurrentUser();
+                try {
+                    mAuth.signInWithEmailAndPassword(User_Name, Password)
+                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        System.out.println(user.getEmail());
+                                        // Sign in success, update UI with the signed-in user's information
 
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Toast.makeText(getApplicationContext(), "Authentication failed.",
-                                            Toast.LENGTH_LONG).show();
 
-                                    // ...
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(getApplicationContext(), "Password or UserName incorrect",
+                                                Toast.LENGTH_LONG).show();
+
+
+                                    }
                                 }
-
-                                // ...
-                            }
-                        });
-
+                            });
+                    // in case of null name if not throw exeption
+                }catch (Exception e){
+                    Toast.makeText(getApplicationContext(), "Login Failed please try again",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         });
+
+
+
+        // call us by email
+        FAB_mail_input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                go_to_mail_send_screen();
+            }
+        });
+
+
+
+
     }
     public void go_sign_up_page(){
         Intent intent = new Intent(this, SignUpPage.class);
         startActivity(intent);
     }
+
+    public void go_to_mail_send_screen(){
+        Mail_Active mail_active = new Mail_Active();
+        mail_active.show(getSupportFragmentManager(),"Text Us");
+    }
+
 
 }
