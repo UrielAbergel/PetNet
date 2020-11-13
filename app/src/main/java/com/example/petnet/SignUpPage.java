790e6c2291b2317b8a7982c7be7638d58dc6252d
OrianHindi <orianhindi44@gmail.com>
2020-11-14 00:30:29 +0200

    change gender+colors to choose

--- app/src/main/java/com/example/petnet/SignUpPage.java
+++ app/src/main/java/com/example/petnet/SignUpPage.java
@@ -1,14 +1,17 @@
 package com.example.petnet;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -31,8 +34,23 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpPage extends AppCompatActivity {
+
+
+    final String PET_COLOR_BLACK = "0";
+    final String PET_COLOR_WHITE = "1";
+    final String PET_COLOR_GRAY = "2";
+    final String PET_COLOR_GOLDEN = "3";
+    final String PET_COLOR_BROWN = "4";
+    final String PET_COLOR_ = "5";
+    final String PET_COLOR = "6";
+    final String PET_COLOR_GAY = "7";
+    final String PET_COLOR_GOLEN = "8";
+
 
 
 
@@ -46,8 +64,10 @@ public class SignUpPage extends AppCompatActivity {
     EditText confirm_password;
     EditText pet_name;
     EditText pet_race;
-    EditText pet_color;
-    EditText pet_gender;
+    Map<String,Integer>  colors;
+    CheckBox cb_colors[];
+    CheckBox pet_gender_male;
+    CheckBox pet_gender_female;
     EditText uniqe_signs;
     ImageButton hide_pass;
     ImageButton hide_confirm_pass;
@@ -69,9 +89,30 @@ public class SignUpPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
         mAuth=FirebaseAuth.getInstance();
+
+        myRef = FirebaseDatabase.getInstance().getReference("users");
+        mStorageRef = FirebaseStorage.getInstance().getReference();
         toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        //initillaize
+
+        colors = new HashMap<>();
+        userToAdd = new User();
+        cb_colors = new CheckBox[9];
+        colors.put(PET_COLOR_BLACK,0);
+        colors.put(PET_COLOR_WHITE,0);
+        colors.put(PET_COLOR_BROWN,0);
+        colors.put(PET_COLOR_GOLDEN,0);
+        colors.put(PET_COLOR_GOLEN,0);
+        colors.put(PET_COLOR_GAY,0);
+        colors.put(PET_COLOR_GRAY,0);
+        colors.put(PET_COLOR_,0);
+        colors.put(PET_COLOR,0);
+
+
 
 
         //buttons
@@ -80,6 +121,7 @@ public class SignUpPage extends AppCompatActivity {
         take_photo_from_gallery = findViewById(R.id.gallery_photo);
         hide_pass = findViewById(R.id.IV_hide_pass);
         hide_confirm_pass = findViewById(R.id.IV_hide_confirm_pass);
+        business_sign_up= findViewById(R.id.TV_Business_signup);
 
 
         //EditTexts
@@ -91,23 +133,139 @@ public class SignUpPage extends AppCompatActivity {
         pet_photo = findViewById(R.id.pet_pic);
         pet_name = findViewById(R.id.ET_pet_name);
         pet_race = findViewById(R.id.ET_pet_race);
-        pet_color = findViewById(R.id.ET_pet_color);
-        pet_gender = findViewById(R.id.ET_pet_gender);
         uniqe_signs = findViewById(R.id.ET_uniqe_signs);
 
+        //CheckBoxs
+        pet_gender_male = findViewById(R.id.CB_pet_gender_male);
+        pet_gender_female= findViewById(R.id.CB_pet_gender_female);
+        cb_colors[0] = findViewById(R.id.CB_pet_color_black); //1000269
+        cb_colors[1] =findViewById(R.id.CB_pet_color_white);
+        cb_colors[2] = findViewById(R.id.CB_pet_color_gray);
+        cb_colors[3] = findViewById(R.id.CB_pet_color_golden);
+        cb_colors[4] =findViewById(R.id.CB_pet_color_brown);
+        cb_colors[5] = findViewById(R.id.CB_pet_color_); //1000269
+        cb_colors[6] =findViewById(R.id.CB_pet_color);
+        cb_colors[7] = findViewById(R.id.CB_pet_color_gay);
+        cb_colors[8] = findViewById(R.id.CB_pet_color_golen);
 
 
 
 
-        userToAdd = new User();
 
 
 
 
-        business_sign_up= findViewById(R.id.TV_Business_signup);
-        myRef = FirebaseDatabase.getInstance().getReference();
-        mStorageRef = FirebaseStorage.getInstance().getReference();
+         // if color[i] == 1 the user choosed the color else didnt choose.
+        cb_colors[0].setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                if(colors.get(PET_COLOR_BLACK) == 0) colors.replace(PET_COLOR_BLACK,1);
+                else colors.replace(PET_COLOR_BLACK,0);
+            }
+        });
 
+
+        cb_colors[1].setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                if(colors.get(PET_COLOR_WHITE) == 0 ) colors.replace(PET_COLOR_WHITE,1);
+                else colors.replace(PET_COLOR_WHITE,0);
+            }
+        });
+
+        cb_colors[4].setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                if(colors.get(PET_COLOR_BROWN) == 0) colors.replace(PET_COLOR_BROWN,1);
+                else colors.replace(PET_COLOR_BROWN,0);
+            }
+        });
+
+        cb_colors[2].setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                if(colors.get(PET_COLOR_GRAY) == 0) colors.replace(PET_COLOR_GRAY,1);
+                else colors.replace(PET_COLOR_GRAY,0);
+            }
+        });
+
+
+        cb_colors[7].setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                if(colors.get(PET_COLOR_GAY) == 0 ) colors.replace(PET_COLOR_GAY,1);
+                else colors.replace(PET_COLOR_GAY,0);
+            }
+        });
+
+        cb_colors[8].setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                if(colors.get(PET_COLOR_GOLEN) == 0 ) colors.replace(PET_COLOR_GOLEN,1);
+                else colors.replace(PET_COLOR_GOLEN,0);
+            }
+        });
+
+
+
+        cb_colors[3].setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                if(colors.get(PET_COLOR_GOLDEN) == 0 ) colors.replace(PET_COLOR_GOLDEN,1);
+                else colors.replace(PET_COLOR_GOLDEN,0);
+            }
+        });
+
+
+        cb_colors[5].setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                if(colors.get(PET_COLOR_) == 0 ) colors.replace(PET_COLOR_,1);
+                else colors.replace(PET_COLOR_,0);
+            }
+        });
+
+        cb_colors[6].setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                if(colors.get(PET_COLOR) == 0 ) colors.replace(PET_COLOR,1);
+                else colors.replace(PET_COLOR,0);
+            }
+        });
+
+
+
+
+
+
+
+        pet_gender_female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean checked = pet_gender_male.isChecked();
+                if(checked) pet_gender_male.setChecked(false);
+                userToAdd.setGender(0);             // 0 means the user is a female.
+            }
+        });
+
+
+        pet_gender_male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean checked = pet_gender_female.isChecked();
+                if(checked) pet_gender_female.setChecked(false);
+                userToAdd.setGender(1);  // 1 means the user is a male.
+            }
+        });
 
 
 
@@ -117,22 +275,24 @@ public class SignUpPage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                if(password.getText().toString()!=confirm_password.getText().toString()) {
-
-                    Toast.makeText(getApplicationContext(), "Password does not equals.", Toast.LENGTH_LONG).show();
-                    password.setError("Passwords dont match");
-                    password.requestFocus();
-                    return;
-                }
+                System.out.println(password.getText().toString() + " , confirm:" + password.getText().toString());
+//                if(password.getText().toString()!=confirm_password.getText().toString()) {
+//
+//                    Toast.makeText(getApplicationContext(), "Password does not equals.", Toast.LENGTH_LONG).show();
+//                    password.setError("Passwords dont match");
+//                    password.requestFocus();
+//                    return;
+//                }
                 String mail = email.getText().toString();
                 String pass = password.getText().toString();
+                userToAdd.setEmail(mail);
 
                  mAuth.createUserWithEmailAndPassword(mail,pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                      @Override
                      public void onSuccess(AuthResult authResult) {
                          FirebaseUser user = mAuth.getCurrentUser();
                          System.out.println("User UID: "  +user.getUid());
-                         userToAdd.setUid(user.getUid());
+                         userToAdd.setUid(user.getEmail());
                      }
                  }).addOnFailureListener(new OnFailureListener() {
                      @Override
@@ -143,20 +303,23 @@ public class SignUpPage extends AppCompatActivity {
                  });
                  userToAdd.setFname(first_name.getText().toString());
                  userToAdd.setLname(last_name.getText().toString());
-               //  userToAdd.setAddress(addres.getText().toString()); check what about address
+//                 userToAdd.setAddress(adress.getText().toString()); check what about address
                // userToAdd.setGender();
-                userToAdd.setEmail(email.getText().toString());
+
                 userToAdd.setPassword(password.getText().toString());
-                userToAdd.setPname(pet_name.getText().toString());
+                userToAdd.setPet_name(pet_name.getText().toString());
+                userToAdd.setColors(colors);
                 //userToAdd.setRace();
 
 
+                System.out.println(userToAdd.toString());
 
 
 
+            myRef.child(userToAdd.getUid()).setValue(userToAdd);
 
-
-             myRef.child("users").child(userToAdd.getUid()).setValue(userToAdd);
+            Intent intent = new Intent(getApplicationContext(),BSignUpPage.class);
+            startActivity(intent);
             }
 //
 //
@@ -235,8 +398,8 @@ public class SignUpPage extends AppCompatActivity {
                 Bitmap imageBitmap = (Bitmap) extras.get("data");
                 Bitmap check = imageBitmap;
                 System.out.println(check  + "BITMAP," + imageBitmap);
-                userToAdd.setPet_pic(imageBitmap);
-               pet_photo.setImageBitmap(imageBitmap);
+
+                pet_photo.setImageBitmap(imageBitmap);
 
                 Toast.makeText(this, "Result ok", Toast.LENGTH_LONG).show();
             } else Toast.makeText(this, "Result failed", Toast.LENGTH_LONG).show();
