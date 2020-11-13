5515d627e5c92c8525d41953b43f22ad0392a495
OrianHindi <orianhindi44@gmail.com>
2020-11-14 00:30:29 +0200

    change gender+colors to choose

--- app/src/main/java/com/example/petnet/User.java
+++ app/src/main/java/com/example/petnet/User.java
@@ -2,134 +2,151 @@ package com.example.petnet;
 
 import android.graphics.Bitmap;
 
+import java.util.Arrays;
+import java.util.Map;
+
 public class User {
 
-    private String fname;
-    private String lname;
-    private String email;
-    private String password;
-    private String address;
-    private int gender;
-    private Bitmap pet_pic;
+    private String Fname;
+    private String Lname;
+    private String Email;
+    private String Password;
+    private String Address;
+    private int Gender;
 
-    private String uid;
-    private String pname;
-    private String race;
-    private int pgender;
-    private int size; // split into categories 1-5
-    private int[] colors; //need to check how many colors we accept and their values.
-    private String uniqe_signs;
+    private String Uid;
+    private String Pet_name;
+    private String Pet_race;
+    private int Pet_gender;
+    private int Size; // split into categories 1-5
+    private Map<String,Integer> colors;
+    private String Uniqe_signs;
 
-
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public Bitmap getPet_pic() {
-        return pet_pic;
-    }
-
-    public void setPet_pic(Bitmap pet_pic) {
-        this.pet_pic = pet_pic;
-    }
-
-    public String getPname() {
-        return pname;
-    }
-
-    public void setPname(String pname) {
-        this.pname = pname;
-    }
-
-    public String getFname() {
-        return fname;
-    }
-
-    public void setFname(String fname) {
-        this.fname = fname;
-    }
-
-    public String getLname() {
-        return lname;
-    }
-
-    public void setLname(String lname) {
-        this.lname = lname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getGender() {
-        return gender;
-    }
-
-    public void setGender(int gender) {
-        this.gender = gender;
-    }
-
-    public String getRace() {
-        return race;
-    }
-
-    public void setRace(String race) {
-        this.race = race;
-    }
-
-    public int getPgender() {
-        return pgender;
-    }
-
-    public void setPgender(int pgender) {
-        this.pgender = pgender;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public int[] getColors() {
+    public Map<String, Integer> getColors() {
         return colors;
     }
 
-    public void setColors(int[] colors) {
+    public void setColors(Map<String, Integer> colors) {
         this.colors = colors;
     }
 
+    public String getFname() {
+        return Fname;
+    }
+
+    public void setFname(String fname) {
+        Fname = fname;
+    }
+
+    public String getLname() {
+        return Lname;
+    }
+
+    public void setLname(String lname) {
+        Lname = lname;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public int getGender() {
+        return Gender;
+    }
+
+    public void setGender(int gender) {
+        Gender = gender;
+    }
+
+
+
+    public String getUid() {
+        return Uid;
+    }
+
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+
+    public String getPet_name() {
+        return Pet_name;
+    }
+
+    public void setPet_name(String pet_name) {
+        Pet_name = pet_name;
+    }
+
+    public String getPet_race() {
+        return Pet_race;
+    }
+
+    public void setPet_race(String pet_race) {
+        Pet_race = pet_race;
+    }
+
+    public int getPet_gender() {
+        return Pet_gender;
+    }
+
+    public void setPet_gender(int pet_gender) {
+        Pet_gender = pet_gender;
+    }
+
+    public int getSize() {
+        return Size;
+    }
+
+    public void setSize(int size) {
+        Size = size;
+    }
+
+
+
+
     public String getUniqe_signs() {
-        return uniqe_signs;
+        return Uniqe_signs;
     }
 
     public void setUniqe_signs(String uniqe_signs) {
-        this.uniqe_signs = uniqe_signs;
+        Uniqe_signs = uniqe_signs;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "Fname='" + Fname + '\'' +
+                ", Lname='" + Lname + '\'' +
+                ", Email='" + Email + '\'' +
+                ", Password='" + Password + '\'' +
+                ", Address='" + Address + '\'' +
+                ", Gender=" + Gender +
+                ", Uid='" + Uid + '\'' +
+                ", Pet_name='" + Pet_name + '\'' +
+                ", Pet_race='" + Pet_race + '\'' +
+                ", Pet_gender=" + Pet_gender +
+                ", Size=" + Size +
+                ", colors=" + colors +
+                ", Uniqe_signs='" + Uniqe_signs + '\'' +
+                '}';
     }
 }
