67d146d7eeb87b590762eb26376157cb91211b57
yairsmadar <yairsemama@gmail.com>
2020-11-25 20:46:33 +0200

    add found dog activity

---
+++ app/src/main/java/com/example/petnet/FoundDogActivity.java
@@ -0,0 +1,83 @@
+package com.example.petnet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class FoundDogActivity extends AppCompatActivity {
+
+    final float loc = 260, add_for_loc = 20;
+    final short loc_huge = 0, loc_big = 1, loc_medium = 2, loc_small = 3, loc_tiny = 4;
+    final double size = 1.2, add_for_size = 0.2;
+    final double size_huge = 0, size_big = 1, size_medium = 2, size_small = 3, size_tiny = 4;
+
+    Button B_dog_size_huge;
+    Button B_dog_size_big;
+    Button B_dog_size_medium;
+    Button B_dog_size_small;
+    Button B_dog_size_tiny;
+    ImageView IV_dog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_found_dog);
+        B_dog_size_huge = findViewById(R.id.B_huge);
+        B_dog_size_big = findViewById(R.id.B_big);
+        B_dog_size_medium = findViewById(R.id.B_medium);
+        B_dog_size_small = findViewById(R.id.B_small);
+        B_dog_size_tiny = findViewById(R.id.B_tiny);
+
+        IV_dog = findViewById(R.id.dog_pic_for_size);
+
+        B_dog_size_huge.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             IV_dog.setScaleX((float) (size - add_for_size*size_huge));
+             IV_dog.setScaleY((float) (size - add_for_size*size_huge));
+             IV_dog.setY(loc + add_for_loc*loc_huge);
+         }
+     });
+
+        B_dog_size_big.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            IV_dog.setScaleX((float) (size - add_for_size*size_big));
+            IV_dog.setScaleY((float) (size - add_for_size*size_big));
+            IV_dog.setY(loc + add_for_loc* loc_big);
+
+        }
+    });
+
+        B_dog_size_medium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IV_dog.setScaleX((float) (size - add_for_size*size_medium));
+                IV_dog.setScaleY((float) (size - add_for_size*size_medium));
+                IV_dog.setY(loc + add_for_loc* loc_medium);
+            }
+        });
+
+        B_dog_size_small.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IV_dog.setScaleX((float)(size - add_for_size*size_small));
+                IV_dog.setScaleY((float)(size - add_for_size*size_small));
+                IV_dog.setY(loc + add_for_loc* loc_small);
+            }
+        });
+
+        B_dog_size_tiny.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IV_dog.setScaleX((float)(size - add_for_size*size_tiny));
+                IV_dog.setScaleY((float)(size - add_for_size*size_tiny));
+                IV_dog.setY(loc + add_for_loc* loc_tiny);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/petnet/UserMainActivity.java
+++ app/src/main/java/com/example/petnet/UserMainActivity.java
@@ -2,13 +2,31 @@ package com.example.petnet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class UserMainActivity extends AppCompatActivity {
+
+    LinearLayout LL_found_a_dogl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_main);
+        LL_found_a_dogl = findViewById(R.id.LL_found_a_dog);
+
+        LL_found_a_dogl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                go_find_a_dog_page();
+            }
+        });
+    }
+
+    public void go_find_a_dog_page(){
+        Intent intent = new Intent(this, FoundDogActivity.class);
+        startActivity(intent);
     }
 }
