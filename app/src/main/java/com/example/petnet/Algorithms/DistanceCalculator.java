aeb96df6b27226ee5e6bb75ae7e7f7d5f19ae3c0
yairsmadar <yairsemama@gmail.com>
2020-12-09 21:34:39 +0200

    add comments

--- app/src/main/java/com/example/petnet/Algorithms/DistanceCalculator.java
+++ app/src/main/java/com/example/petnet/Algorithms/DistanceCalculator.java
@@ -1,35 +1,24 @@
 package com.example.petnet.Algorithms;
 import java.lang.*;
 
-
-/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-/*::                                                                         :*/
-/*::  This routine calculates the distance between two points (given the     :*/
-/*::  latitude/longitude of those points). It is being used to calculate     :*/
-/*::  the distance between two locations using GeoDataSource (TM) products   :*/
-/*::                                                                         :*/
-/*::  Definitions:                                                           :*/
-/*::    Southern latitudes are negative, eastern longitudes are positive     :*/
-/*::                                                                         :*/
-/*::  Function parameters:                                                   :*/
-/*::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :*/
-/*::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :*/
-/*::    unit = the unit you desire for results                               :*/
-/*::           where: 'M' is statute miles (default)                         :*/
-/*::                  'K' is kilometers                                      :*/
-/*::                  'N' is nautical miles                                  :*/
-/*::  Worldwide cities and other features databases with latitude longitude  :*/
-/*::  are available at https://www.geodatasource.com                         :*/
-/*::                                                                         :*/
-/*::  For enquiries, please contact sales@geodatasource.com                  :*/
-/*::                                                                         :*/
-/*::  Official Web site: https://www.geodatasource.com                       :*/
-/*::                                                                         :*/
-/*::           GeoDataSource.com (C) All Rights Reserved 2019                :*/
-/*::                                                                         :*/
-/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 public class DistanceCalculator
 {
+    /**
+     * This routine calculates the distance between two points (given the
+     * latitude/longitude of those points). It is being used to calculate
+     * the distance between two locations using GeoDataSource (TM) products
+     *
+     * Definitions:
+     *      Southern latitudes are negative, eastern longitudes are positive
+     * Function parameters:
+     *      lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)
+     *      lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)
+     *      unit = the unit you desire for results
+     *           where: 'M' is statute miles (default)
+     *                  'K' is kilometers
+     *                  'N' is nautical miles
+     * @return the distance between the two given coordinators
+     */
     public static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
         if ((lat1 == lat2) && (lon1 == lon2)) {
             return 0;
